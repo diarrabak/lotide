d88143166557bfb2fb10d8138a8bb8e48bf7def6
diarrabak <71572448+diarrabak@users.noreply.github.com>
2021-11-14 10:55:12 +0000

    The first commit

---
+++ assertEqual.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ ✅ ✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴 🔴 🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE SCENARIOS
+
+//Comparing identical strings
+
+assertEqual("Bootcamp", "Bootcamp");
+//Comparing non-identical strings
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+
+//Comparing identical numbers
+
+assertEqual(1, 1);
+//Comparing non-identical numbers
+
+assertEqual(1, 5);
